42c95625f2762f8f840421eab845048cfa921018
Austin DeGarmo <austindegarmo@yahoo.com>
2017-06-19 23:46:32 -0700

    midnight push

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+// define variables
+// computerGuess -randomly generated number between 25 and 100
+$('.start')on('click', function(){
+        start();
+    })
+
+var blueCrystal = Math.floor(Math.random()*15 + 1);
+
+var redCrystal = Math.floor(Math.random()*15 + 1);
+
+var amethystCrystal = Math.floor(Math.random()*15 + 1);
+
+var onyxCrystal = Math.floor(Math.random()*15 + 1);
+
+var wins = 0;
+
+var losses = 0;
+
+var score = 0;
+
+var computerGuess = Math.floor(Math.random()*100 + 25);
+
+
+
+
+
+$('#black crystal').click(function() {
+	 if score++ 
+}
+
+$('#compGuess').html(computerGuess);
+	// = score += onyxCrystal;
+	// // console.log(score);
+	// $('#userScore').html(score);
+	// checkWins();
